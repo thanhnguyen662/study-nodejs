9e54b74366fc4d924dd9a9eebb88f9e5818483d3
The Thanh <thethanhnguyen.662@gmail.com>
2021-03-17 10:31:06 +0700

    add scss lib :tada:

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+
+app.use(express.static(path.join(__dirname, 'public')))
+
 //http logger
 app.use(morgan('combined'))
 
@@ -14,8 +17,7 @@ app.engine('hbs', handlebars({
   extname: '.hbs'
 }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources\\views'))
-
+app.set('views', path.join(__dirname, 'resources','views'))
 
 
 app.get('/', (req, res) => {
