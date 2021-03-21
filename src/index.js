cf44dbc739959d8d7a15e3afac344834d82c2cf6
The Thanh <thethanhnguyen.662@gmail.com>
2021-03-21 17:26:05 +0700

    123

--- src/index.js
+++ src/index.js
@@ -12,25 +12,27 @@ const db = require('./config/db');
 //connect db
 db.connect();
 
-app.use(express.static(path.join(__dirname, 'public')))
-    app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //http logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 //templates engine
-app.engine('hbs', handlebars({
-  extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources','views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 //routes init
 route(app);
 
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})      
+    console.log(`Example app listening at http://localhost:${port}`);
+});
